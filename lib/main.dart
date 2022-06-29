2a405bdd0a5e42a44a279044b0931c2d16b94176
FlutLab.io <>
2022-06-29 13:11:06 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'home.dart';
-import 'login-register.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutterfire_samples/screens/login-register.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(MyApp());
 }
 
-final FirebaseAuth auth = FirebaseAuth.instance;
-
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //title: 'GO',
+      title: 'FlutterFire Samples',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(hintColor: Color(0xFFC0F0E8), primaryColor: Color(0xFF80E1D1), fontFamily: "Montserrat", canvasColor: Colors.transparent),
-      home: new StreamBuilder(
-        stream: auth.onAuthStateChanged,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Home();
-          }
-          return LoginRegister();
-        },
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        brightness: Brightness.dark,
       ),
-      routes: <String, WidgetBuilder>{
-        '/home': (BuildContext context) => new Home(),
-        '/login': (BuildContext context) => new LoginRegister()
-      },
+      home: LoginScreen(),
     );
   }
 }
