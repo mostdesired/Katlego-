2c51f49585e5c5c1a0e5e79c516a17fce248555f
FlutLab.io <>
2022-07-09 21:59:17 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -9,31 +9,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          body: Container(
-              child: Column(children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(15),
-          alignment: Alignment.topLeft,
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: 'name',
-              border: OutlineInputBorder(),
-            ),
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.all(15),
-          alignment: Alignment.topRight,
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: 'age',
-              border: OutlineInputBorder(),
-            ),
-          ),
-        ),
-      ]))),
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Row(
+              //Row(
+              children: <Widget>[
+                Expanded(
+                  child: Row(children: <Widget>[
+                    Expanded(
+                      flex: 3,
+                      child: TextField(
+                        decoration: InputDecoration(hintText: 'Time'),
+                      ),
+                    ),
+                  ]),
+                ),
+              ]),
+        ));
   }
 }
