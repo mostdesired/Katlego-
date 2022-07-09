661dd4a14b2f449f21bd1d3af06d242651c82442
FlutLab.io <>
2022-07-09 17:31:33 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'flutter demo',
-      //home: MyApplication(),
+      title: 'Hello World',
     );
   }
-}
-
-//class MyApplication extends StatefulWidget {
-//const MyApplication({Key? key}) : super(key: key);
-
-//@override
-//_MyAppState createState() => new _MyAppState();
-//}
-
-//class _MyAppState extends State<MyApplication> {
-//@override
-//void initState() {
-// super.initState();
-//Timer(Duration(seconds: 3), () => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context) => MyStatefulWidget())));
-//}
-
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: Padding(
-      padding: EdgeInsets.only(top: 90, bottom: 65),
-      child: Container(
-          child: Center(
-        child: Image.asset("images/icon.jpg"),
-      )),
-    ),
-  );
 }
