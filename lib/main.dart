49fc99dea5710682bc3d835c74c16f48e9c5fb0e
FlutLab.io <>
2022-07-09 19:02:04 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
+        body: Container(
           child: Text('Hello world'),
         ),
       ),
