7bf398e0567e340c78af65596a80c556602c4818
FlutLab.io <>
2022-07-09 17:04:46 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
-import 'login-register.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterFire Samples',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        //brightness: Brightness.dark,
-      ),
-      home: LoginScreen(),
+      title: 'flutter demo',
+      //home: MyApplication(),
     );
   }
+}
+
+class MyApplication extends StatefulWidget {
+  const MyApplication({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+//class _MyAppState extends State<MyApplication> {
+//@override
+//void initState() {
+// super.initState();
+//Timer(Duration(seconds: 3), () => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context) => MyStatefulWidget())));
+//}
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: Padding(
+      padding: EdgeInsets.only(top: 90, bottom: 65),
+      child: Container(
+          child: Center(
+        child: Image.asset("images/icon.jpg"),
+      )),
+    ),
+  );
 }
