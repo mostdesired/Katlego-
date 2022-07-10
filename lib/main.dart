8ca03132c17324c0b5e3b71a4deb3e60f902d08c
FlutLab.io <>
2022-07-10 19:34:12 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -9,41 +9,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          body: Row(
-               mainAxisAlignment: MainAxisAlignment.spaceEvenly,  
-          children:<Widget>[  
-            Container(  
-              margin: EdgeInsets.all(12.0),  
-              padding: EdgeInsets.all(8.0),  
-              decoration:BoxDecoration(  
-                  borderRadius:BorderRadius.circular(8),  
-                  color:Colors.green  
-              ),  
-              child: TextField(
-                decoration:InputDecoration(
-                  border:OutlineInputBorder(),
-                  labelText:'name',
-                ),
-              ),  
-           ),
-
-           Container(  
-              margin: EdgeInsets.all(12.0),  
-              padding: EdgeInsets.all(8.0),  
-              decoration:BoxDecoration(  
-                  borderRadius:BorderRadius.circular(8),  
-                  color:Colors.green  
-              ),  
-              child: TextField(
-                decoration:InputDecoration(
-                  border:OutlineInputBorder(),
-                  labelText:'Age',
-                ),
-              ),
-           ),
-          ]),
-        );
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(12.0),
+          padding: EdgeInsets.all(8.0),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(8), color: Colors.green),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'name',
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.all(12.0),
+          padding: EdgeInsets.all(8.0),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(8), color: Colors.green),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Age',
+            ),
+          ),
+        ),
+      ])),
+    );
   }
 }
