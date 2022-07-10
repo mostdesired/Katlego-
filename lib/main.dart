84932fc6a925cccc9e158e8fbcd0bfcd1ca83fb2
FlutLab.io <>
2022-07-10 19:31:14 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -12,23 +12,38 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: Scaffold(
           body: Row(
-              //Row(
-              children: <Widget>[
-                Expanded(
-                  child: Row(children: <Widget>[
-                    Expanded(
-                      flex: 3,
-                      child: TextField(
-                        decoration: InputDecoration(hintText: 'Time'),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 4,
-                      child: Text("(in mins)/"),
-                    ),
-                  ]),
+               mainAxisAlignment: MainAxisAlignment.spaceEvenly,  
+          children:<Widget>[  
+            Container(  
+              margin: EdgeInsets.all(12.0),  
+              padding: EdgeInsets.all(8.0),  
+              decoration:BoxDecoration(  
+                  borderRadius:BorderRadius.circular(8),  
+                  color:Colors.green  
+              ),  
+              child: TextField(
+                decoration:InputDecoration(
+                  border:OutlineInputBorder(),
+                  labelText:'name',
                 ),
-              ]),
-        ));
+              ),  
+           ),
+
+           Container(  
+              margin: EdgeInsets.all(12.0),  
+              padding: EdgeInsets.all(8.0),  
+              decoration:BoxDecoration(  
+                  borderRadius:BorderRadius.circular(8),  
+                  color:Colors.green  
+              ),  
+              child: TextField(
+                decoration:InputDecoration(
+                  border:OutlineInputBorder(),
+                  labelText:'Age',
+                ),
+              ),
+           ),
+          ]),
+        );
   }
 }
